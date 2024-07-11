6099289c572e705c43c6b0e1c34e1daaae200245
Matheus Ryuki <matheuskaiya2@gmail.com>
2024-07-11 09:20:05 -0300

    usando parametros para passar dados entre telas

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,20 +1,21 @@
 import { StyleSheet, Text, View } from "react-native";
 import { MEALS } from "../data/dummy-data";
 
-const MealsOverviewScreen = () => {
-    return <View style={styles.container}>
-        <Text>
-            Refeições
-        </Text>
+const MealsOverviewScreen = ({ route }) => {
+  const catId = route.params.categoryId;
+
+  return (
+    <View style={styles.container}>
+      <Text>Refeições - {catId}</Text>
     </View>
+  );
 };
 
 export default MealsOverviewScreen;
 
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 16
-    }
-})s
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
