c0660656713712ce8429a8b2c80fd32409f3bce4
Matheus Ryuki <matheuskaiya2@gmail.com>
2024-07-12 13:17:15 -0300

    gerenciando com o context

--- store/context/favorite-context.js
+++ store/context/favorite-context.js
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const FavoritesContext = createContext({
   ids: [],
@@ -7,7 +7,29 @@ export const FavoritesContext = createContext({
 });
 
 const FavoritesContextProvider = ({ children }) => {
-  return <FavoritesContext>{children}</FavoritesContext>;
+  const [favoriteMealsId, setFavoritesMealsId] = useState([]);
+
+  const addFavorite = (id) => {
+    setFavoritesMealsId((prevState) => [...prevState, id]);
+  };
+
+  const removeFavorite = (id) => {
+    setFavoritesMealsId((prevState) =>
+      prevState.filter((mealId) => mealId !== id)
+    );
+  };
+
+  const value = {
+    ids: favoriteMealsId,
+    addFavorite,
+    removeFavorite,
+  };
+
+  return (
+    <FavoritesContext.Provider value={value}>
+      {children}
+    </FavoritesContext.Provider>
+  );
 };
 
 export default FavoritesContextProvider;
